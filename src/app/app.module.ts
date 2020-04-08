e99f32131f45538182de5e584ed36941cdd71c67
edwin0003 <edwincrespomarin@gmail.com>
2020-04-08 13:28:58 -0400

    Add componets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { HomeComponent } from './home/home.component';
+import { PerfilComponent } from './perfil/perfil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    HomeComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule,
