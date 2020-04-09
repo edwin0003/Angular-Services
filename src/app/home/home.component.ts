88f7961830892118c2066606ecad773a6f22c4d3
edwin0003 <edwincrespomarin@gmail.com>
2020-04-09 11:15:07 -0400

    agradando interfaz de articulo y usando servicios usuario

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
+import { Usuario } from './../models/usuario';
 import { Component, OnInit } from '@angular/core';
+import { Articulo } from '../models/articulo';
+import { UsuarioService } from '../services/usuario.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  articulos: Array <Articulo>= new Array<Articulo>();
+
+  constructor(public usuarioInyectado: UsuarioService) { }
 
   ngOnInit(): void {
+    this.articulos.push({
+      titulo: 'Curso JavaScript',
+      descripcion:'hola este es un articulo de prueba de un articulo',
+      fecha: new Date(),
+      usuario: `${this.usuarioInyectado.usuario.nombre} ${this.usuarioInyectado.usuario.apellido}`
+    },
+    {
+      titulo: 'Curso React',
+      descripcion:'hola este es un articulo de prueba de un articulo',
+      fecha: new Date('01/10/2020'),
+      usuario: `${this.usuarioInyectado.usuario.nombre} ${this.usuarioInyectado.usuario.apellido}`
+    },
+    {
+      titulo: 'Curso Vuejs',
+      descripcion:'hola este es un articulo de prueba de un articulo',
+      fecha: new Date('06/26/2020'),
+      usuario: `${this.usuarioInyectado.usuario.nombre} ${this.usuarioInyectado.usuario.apellido}`
+    },
+    )
   }
 
 }
---
+++ src/app/models/articulo.ts
@@ -0,0 +1,7 @@
+import { Usuario } from './usuario';
+export interface Articulo{
+    titulo:string;
+    descripcion: string;
+    fecha: Date;
+    usuario: string
+}
