c6a1b9dac4b4ca8a0867090ffd2206229acc8904
edwin0003 <edwincrespomarin@gmail.com>
2020-04-10 17:43:02 -0400

    Haciendo uso de apiRest Online

--- src/app/articulo-detalle/articulo-detalle.component.ts
+++ src/app/articulo-detalle/articulo-detalle.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { ArticulosService } from '../services/articulos.service';
-import { Articulo } from '../models/articulo';
+import { Component, OnInit } from "@angular/core";
+import { ArticulosService } from "../services/articulos.service";
+import { Articulo } from "../models/articulo";
+import { User } from "../models/user";
 
 @Component({
-  selector: 'app-articulo-detalle',
-  templateUrl: './articulo-detalle.component.html',
-  styleUrls: ['./articulo-detalle.component.scss']
+  selector: "app-articulo-detalle",
+  templateUrl: "./articulo-detalle.component.html",
+  styleUrls: ["./articulo-detalle.component.scss"],
 })
 export class ArticuloDetalleComponent implements OnInit {
-  articulo: Articulo= new Articulo();
+  articulo: Articulo = new Articulo();
+  usuario: User = new User();
   constructor(private articuloInyectado: ArticulosService) {
-    this.articulo= this.articuloInyectado.articulo 
-   }
-
-  ngOnInit(): void {
+    this.articulo = this.articuloInyectado.articulo;
   }
 
+  ngOnInit(): void {
+    this.articuloInyectado
+      .leerUsuario(this.articulo.userId)
+      .subscribe((usuarioDesdeApi) => {
+        this.usuario = usuarioDesdeApi
+      });
+  }
 }
---
+++ src/app/models/user.ts
@@ -0,0 +1,9 @@
+import { Address } from './address';
+
+export class User{
+    id:number;
+    name:string
+    email:string;
+    address: Address;
+    username: string
+}
